2488aaf2d29ccc70ad291b5b63ce11e0706c3ded
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-08-07 20:40:49 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://35.179.160.11:9090/springapp1';
+const BASE_URL = 'http://13.51.171.75:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -49,7 +49,7 @@ const BASE_URL = 'http://35.179.160.11:9090/springapp1';
  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://35.179.160.11:9090/springapp1/delete/${id}`);
+      await axios.delete(`http://13.51.171.75:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
